1fd4c9e8e39060e6d71b1c13cad19ccb9299b1d7
andrewLA331 <andrew.allen2@yahoo.com>
2016-03-24 17:36:38 -0400

    Added files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Redesign Index</title> 
+    
+</head>
+<body>
+<img src="layout.JPG" alt="Layout" style="width:800px;height:600px;">
+
+    <p>We wanted a simple and modern layout that relies on people's existing familiarity with other professional websites. Since this is a website used for educational purposes we wanted students to be able to easily navigate the different sections and to have all the information well organized and legible. </p>
+</body>
